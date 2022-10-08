fd90786cccb323ac10208f3e9e9acd9b0d3658a8
kishanpandav <pandavkishan60@gmail.com>
2022-10-08 11:13:04 +0530

    Icon change

--- lib/main copy.dart
+++ lib/main copy.dart
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.abc_rounded),
+        child: const Icon(Icons.ac_unit),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
